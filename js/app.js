80c28f574b77c3a21f2c098aa87c0e3aece4455e
TheYogi1999 <96660471+TheYogi1999@users.noreply.github.com>
2026-09-23 00:03:00 +0200

    Reduce vintage text shadow offset

--- js/app.js
+++ js/app.js
@@ -490,8 +490,8 @@
         ? new fabric.Shadow({
             color: 'rgba(0,0,0,0.75)',
             blur: 2,
-            offsetX: -4,
-            offsetY: 6,
+            offsetX: -1,
+            offsetY: 1.5,
             affectStroke: false,
           })
         : null);
